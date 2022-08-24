86822a05bea23d83060d83e6296d63c955a812e0
MrAhmedElsayed <ahmedsoftwaredevelopment@gmail.com>
2022-08-25 01:27:25 +0300

    down grade

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeView from './views/Home';
-import SearchPage from './views/Search';
-import NotFound from './views/NotFound';
-import SignIn from './views/SignIn';
+import SearchView from "./SearchView";
 
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+ReactDOM.render(
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<HomeView />} />
-        <Route path="search" element={<SearchPage />} />
-        <Route path='sign-in' element={<SignIn />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="search" element={<SearchView />} />
       </Route>
     </Routes>
-  </BrowserRouter>
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
